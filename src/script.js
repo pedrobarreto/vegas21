fb1097c25ae432541825b95b9afd6e38d0153737
Pedro Barreto <pbdeoliveira@gmail.com>
2021-08-26 00:40:00 -0300

    ajuste cartas

--- src/script.js
+++ src/script.js
@@ -9,56 +9,45 @@ const deckid = deckData.deck_id;
 return deckid;
 }
 
-
-
-
-
 const deckGet = async (deckid, count) => { 
   const fetchDeck = await fetch(`https://deckofcardsapi.com/api/deck/${deckid}/draw/?count=${count}`)
   const deck = await fetchDeck.json();
- return deck.cards.map(({ code, image, suit, value }) => {
-  // createCards({ image, suit, value }); 
- return createCards({ code, image, suit, value });
+ return deck.cards.map(({ code, image, suit, value }, i) => {
+
+ return createCards({ code, image, suit, value, i});
   })
 }
 
 const table = async () => { 
-   const  cardTb = await deckGet(g, 5)
-   console.log(cardTb)
+  const count = 5;
+   const  cardTb = await deckGet(g, count)
    cardTb.forEach((card) => {
      tableCards.appendChild(card)
-
    })
   }
 
    const player = async () => { 
-    const  cardTb = await deckGet(g, 1)
-    console.log(cardTb)
-    cardTb.forEach((card) => {
+    const count = 2;
+    const  cardTb = await deckGet(g, count)
+   return cardTb.forEach((card) => {
       playerCards.appendChild(card)
- 
     })
   }
-  // tableCards.appendChild(deckGet(g, 4));
 
-
-  
-
-//   const player = () => { 
-//     playerCards.appendChild(deckGet(1));
-//     }
-//     player();
-
-
-
-    function createCards({ code, image, suit, value }) {
-      const section = document.createElement('section');
-      section.className = 'card';
-      section.appendChild(createCustomElement('span', 'card__value', value));
-      section.appendChild(createCustomElement('span', 'card__suit', suit));
+    function createCards({ code, image, suit, value, i}) {
+      console.log(i)
+     
+       const section = document.createElement('section');
+      section.className = `card-${code}`;
+      section.style.zIndex = i + 1
+      section.style.left = (i * 90 ) + 'px'
+      section.style.position = 'absolute'
+      section.appendChild(createCustomElement('span', value));
+      section.appendChild(createCustomElement('span', suit));
       section.appendChild(createCardImageElement(image));
     
       return section;
+
     }
 
     function createCardImageElement(imageSource) {
@@ -68,17 +57,14 @@ const table = async () => {
       return img;
     }
 
-
-function createCustomElement(element, className, innerText) {
+function createCustomElement(element, className) {
   const e = document.createElement(element);
   e.className = className;
-  e.innerText = innerText;
   return e;
 }
 
   window.onload = async () => {
  g = await deckDraw();
-await  table()
-await player()
-
+    await table()
+    await player()
   }
